998a105c1542129ad62d8d544dd9d20fa97ed18e
Paulo <paulohenriquesms10@gmail.com>
2023-01-14 16:27:26 -0300

    add: querySelector and querySelectorAll

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ console.log(document.getElementById('author'))
 console.log(document.getElementsByClassName('insta'))
 
 console.log(document.getElementsByTagName('h2'))
+
+console.log(document.querySelector("h1"))
+
+console.log(document.querySelectorAll("h1"))
